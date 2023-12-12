090ec5e95a7b82b12eafa53246591f6ccecd9cf6
ch <mkmsacc@outlook.com>
2023-12-12 10:26:49 +0700

    Update Spring dependencies and Java version in build.gradle.kts
    Updated the version of Spring Boot and Spring Dependency Management used in the project. The changes also introduced a new plugin, Openrewrite, increased the source compatibility from Java 17 to 21 and added a script block for Openrewrite tasks.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.1.2"
-	id("io.spring.dependency-management") version "1.1.2"
+	id("org.springframework.boot") version "3.2.0"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("org.openrewrite.rewrite") version("6.5.12")
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_21
 }
 
 configurations {
@@ -31,4 +32,8 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+rewrite {
+	activeRecipe("org.openrewrite.java.format.AutoFormat","org.openrewrite.java.RemoveUnusedImports")
 }
